2bb9651af47a430eec5ddc4dc58f7c3027fe06d2
Hanzala Arfat <hanzala735@gmail.com>
2021-07-30 10:36:54 +0530

    some changes

--- server.js
+++ server.js
@@ -22,10 +22,17 @@ app.use("/api/users", userRoutes);
 // --------------------------deployment------------------------------
 const __dirname = path.resolve();
 
-app.get("/", (req, res) => {
-  res.send("API is running..");
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 // --------------------------deployment------------------------------
 
 // Error Handling middlewares
@@ -36,5 +43,8 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running in mode on port ${PORT}..`.yellow.bold)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}..`.yellow
+      .bold
+  )
 );

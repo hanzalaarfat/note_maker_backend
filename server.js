bd60671fb49da4cc519d4bdf0a765d30b9447f84
Hanzala Arfat <hanzala735@gmail.com>
2021-07-30 11:10:43 +0530

    some changes

--- server.js
+++ server.js
@@ -23,10 +23,10 @@ app.use("/api/users", userRoutes);
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "/Frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "Frontend", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
